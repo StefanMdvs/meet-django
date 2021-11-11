b2f9c8744b3f7549b22ccf87381317781d106b40
StefanMdvs <stefan.medves@gmail.com>
2021-11-11 14:10:10 +0000

    Add missing bracket

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-%srq5#uid6oz6ddjs9u@w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
